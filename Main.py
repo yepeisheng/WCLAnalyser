1b76aa36b30e56a7bce3568a871d1dc30c235f65
Ye Peisheng <ye_p@worksap.co.jp>
2017-10-10 19:24:24 +0900

    Sample code to fetch data
    Empty

--- Main.py
+++ Main.py
@@ -1,14 +1,14 @@
 import json
 import Fetcher
 import Inspector
-
+import os
+import time
 
 Goroth_dps_rank_var = {
     "encounterId": 2032,
     "metric": "dps",
     "difficulty": 5,
     "partition": 2,
-    "limit": 1,
     "page": 1
 }
 
@@ -25,9 +25,38 @@ DH_ability_crit = {
 Goroth_DH_dps_rank_var = Goroth_dps_rank_var.copy()
 Goroth_DH_dps_rank_var.update(DH_rank_var)
 
-rankings = Fetcher.fetch_ranking(Goroth_DH_dps_rank_var)["rankings"]
+target_dir = "data/"+time.strftime("%Y.%d.%m")
+if not os.path.exists(target_dir):
+    os.makedirs(target_dir)
 
-for dh in rankings:
+with open(target_dir+"/ranking.txt", "w") as rankingFile:
+    rankingFile.write("#, report_id, fight_id, player_id, fight_start, fight_end\n")
+    count = 0
+    page = 0
+    while count < 3000:
+        page+=1
+        Goroth_DH_dps_rank_var["page"] = page
+        rankings = Fetcher.fetch_ranking(Goroth_DH_dps_rank_var)["rankings"]
+        for dh in rankings:
+            if Inspector.is_auto_trans(dh):
+                #Fetch fight data
+                reportID = dh["reportID"]
+                fightID = dh["fightID"]
+                playerName = dh["name"]
+                fights, friendlies = Fetcher.fetch_fights(reportID)
+                fight, player = Inspector.extract_player_fight(fights, friendlies, fightID, playerName)
+                #Extra fight data
+                fight_start = fight["start_time"]
+                fight_end = fight["end_time"]
+                player_id = player["id"]
+                #Generate output
+                outputline = str(count)+", "+reportID+", "+str(fightID)+", "+str(player_id)+", "+str(fight_start)+", "+str(fight_end)+"\n"
+                print outputline
+                rankingFile.write(outputline)
+                count+=1
+
+
+for dh in range(0,0):
     reportID = dh["reportID"]
     fightID = dh["fightID"]
     playerName = dh["name"]
@@ -37,6 +66,7 @@ for dh in rankings:
 
     fights, friendlies = Fetcher.fetch_fights(reportID)
     fight, player = Inspector.extract_player_fight(fights, friendlies, fightID, playerName)
+
     events = Fetcher.fetch_events(reportID,  fight["start_time"], fight["end_time"], player["id"])
     with open("test/events.json", "w") as eventfile:
         json.dump(events, eventfile, indent=4)
